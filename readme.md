fb76c59522147b4bb79113eaf43c1ae3f1f280d4
Maslakov Alexander <jmas.ukraine@gmail.com>
2018-09-24 17:54:37 +0300

    Add readme, cleanup

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Todo List Example with PHP + Stimulus + Turbolinks
+
+Here is small example that demonstrate basic approach of create simple
+Todo application using Stimulus Framework and Turbolinks.
+
+## The Goal
+
+1. Make an application that mix Stimulus and Turbolinks;
+2. Application should feels like Single Page Application;
+3. Application should use minimum of JavaScript on the Application level;
+4. Application should use progressive enhancement web-design strategy;
+5. Application should be server-side rendered.
--- src/app.php
+++ src/app.php
@@ -18,10 +18,7 @@ $container = $app->getContainer();
 
 // Register component on container
 $container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates', [
-        // 'cache' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'templates_cache'
-    ]);
-    // Instantiate and add Slim specific extension
+    $view = new \Slim\Views\Twig( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates');
     $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
     $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
     return $view;
